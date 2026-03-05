10b3de28c218623d3ae56e90f5e13688f54d24be
trdyer06 <trdyer06@gmail.com>
2026-03-04 20:08:47 -0500

    Update TaskManager.java

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,8 +1,30 @@
 public interface TaskManager extends TaskManagerKernel {
 
+    /**
+     * Creates a {@code TaskManager} of {@code Task}s from {@code this} which
+     * have a status of incomplete and reports the {@code TaskManager}.
+     *
+     * @return the {@code TaskManager} containing {@code Task}s with a status of
+     *         incomplete
+     */
     TaskManager incompleteTasks();
 
+    /**
+     * Creates a {@code TaskManager} of {@code Task}s from {@code this} which
+     * have a cateogory named {@code c} and reports the {@code TaskManager}.
+     *
+     * @param c
+     *            the name of the category to be searched for
+     * @return the {@code TaskManager} containing {@code Task}s with a category
+     *         named {@code c}
+     */
     TaskManager tasksInCategory(String c);
 
+    /**
+     * Creates a {@code TaskManager} of the {@code Task}s in {@code this} sorted
+     * by their dates and reports the {@code TaskManager}.
+     *
+     * @return the {@code TaskManager} containing {@code Task}s sorted by date
+     */
     TaskManager tasksByDate();
 }
