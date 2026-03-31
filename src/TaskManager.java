eeda3642c70c9def351e719b6a7becff26cd93b4
trdyer06 <trdyer06@gmail.com>
2026-03-31 15:16:36 -0400

    Update TaskManager.java

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -9,19 +9,23 @@ public interface TaskManager extends TaskManagerKernel {
      *
      * @return the {@code TaskManager} containing {@code Task}s with a status of
      *         incomplete
-     * @ensures incompleteTasks = all Tasks with a status of incomplete in this
+     * @updates this
+     * @ensures incompleteTasks = all Tasks with a status of incomplete in #this
+     *          and this = #this \ incompleteTasks
      */
     TaskManager incompleteTasks();
 
     /**
      * Creates a {@code TaskManager} of {@code Task}s from {@code this} which
-     * have a cateogory named {@code c} and reports the {@code TaskManager}.
+     * have a category named {@code c} and reports the {@code TaskManager}.
      *
      * @param c
      *            the name of the category to be searched for
      * @return the {@code TaskManager} containing {@code Task}s with a category
      *         named {@code c}
-     * @ensures tasksInCategory = all Tasks with a category of c in this
+     * @updates this
+     * @ensures tasksInCategory = all Tasks with a category of c in #this and
+     *          this = #this \ tasksInCategory
      */
     TaskManager tasksInCategory(String c);
 
@@ -30,7 +34,8 @@ public interface TaskManager extends TaskManagerKernel {
      * by their dates and reports the {@code TaskManager}.
      *
      * @return the {@code TaskManager} containing {@code Task}s sorted by date
-     * @ensures tasksByDate = all Tasks in this sorted by date in ascending
+     * @clears this
+     * @ensures tasksByDate = all Tasks in #this sorted by date in ascending
      *          order
      */
     TaskManager tasksByDate();
